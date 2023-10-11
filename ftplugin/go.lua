5708b9c2fd7fb16ea1ed34c926deece9185c01e2
Johan Rönkkö <johan.ronkko@formulate.app>
2023-10-12 00:08:37 +0200

    add ftplugin

---
+++ ftplugin/go.lua
@@ -0,0 +1,11 @@
+local vo = vim.opt_local
+vo.tabstop = 4
+vo.shiftwidth = 4
+vo.softtabstop = 4
+vo.expandtab = false
+
+vim.keymap.set('n', '<leader>xt', function()
+  require('dap-go').debug_test()
+end, { desc = 'Debug closest test' })
+
+vim.cmd 'Copilot enable'
