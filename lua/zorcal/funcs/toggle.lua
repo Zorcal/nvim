e8b5319b2bc1418dcf8183648b31bfcb2f2f9089
Zorcal <zorcal.dev@gmail.com>
2024-01-07 09:07:07 +0100

    refactor

--- lua/zorcal/funcs/toggle.lua
+++ lua/zorcal/funcs/toggle.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 M.relativenumber = function()
-  local opt = 'relativenumber'
-  local v = vim.api.nvim_get_option_value(opt, {})
-  vim.api.nvim_set_option_value(opt, not v, {})
+  local name = 'relativenumber'
+  local v = vim.api.nvim_get_option_value(name, {})
+  vim.api.nvim_set_option_value(name, not v, {})
 end
 
 M.background = function()
-  local opt = 'background'
-  local v = vim.api.nvim_get_option_value(opt, {})
+  local name = 'background'
+  local v = vim.api.nvim_get_option_value(name, {})
   if v == 'dark' then
-    vim.api.nvim_set_option_value(opt, 'light', {})
+    vim.api.nvim_set_option_value(name, 'light', {})
   else
-    vim.api.nvim_set_option_value(opt, 'dark', {})
+    vim.api.nvim_set_option_value(name, 'dark', {})
   end
 end
 
