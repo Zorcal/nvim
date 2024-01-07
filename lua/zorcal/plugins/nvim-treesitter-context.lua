218f5014eb8f027257a94b4f19828061f834b619
Zorcal <zorcal.dev@gmail.com>
2024-01-07 09:07:07 +0100

    update treesitter text objects keybindings

--- lua/zorcal/plugins/nvim-treesitter-context.lua
+++ lua/zorcal/plugins/nvim-treesitter-context.lua
@@ -20,60 +20,6 @@ return {
         zindex = 20, -- The Z-index of the context window
         on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
       }
-      require('nvim-treesitter.configs').setup {
-        textobjects = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['aa'] = '@paramater.outer', -- 'a' for argument
-            ['ia'] = '@paramater.inner',
-            ['ac'] = '@comment.outer',
-            ['ic'] = '@comment.inner',
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']f'] = '@function.outer',
-              [']a'] = '@parameter.inner',
-              [']c'] = '@comment.outer',
-            },
-            goto_next_end = {},
-            goto_previous_start = {
-              ['[f'] = '@function.outer',
-              ['[a'] = '@parameter.inner',
-              ['[c'] = '@comment.outer',
-            },
-            goto_previous_end = {},
-            -- Below will go to either the start or the end, whichever is closer.
-            -- Use if you want more granular movements
-            -- Make it even more gradual by adding multiple queries and regex.
-            goto_next = {},
-            goto_previous = {},
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<leader>za'] = '@parameter.inner',
-            },
-            swap_previous = {
-              ['<leader>zA'] = '@parameter.inner',
-            },
-          },
-        },
-      }
-
-      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
-      -- vim way: ; goes to the direction you were moving.
-      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
-      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
-      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-      vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f)
-      vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F)
-      vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t)
-      vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T)
     end,
   },
 }
--- lua/zorcal/plugins/nvim-treesitter.lua
+++ lua/zorcal/plugins/nvim-treesitter.lua
@@ -18,59 +18,82 @@ return {
           enable = true,
           additional_vim_regex_highlighting = false,
         },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          'go',
-          'gomod',
-          'gosum',
-          'gowork',
-          'rust',
-          'ron',
-          'sql',
-          'python',
-          'c',
-          'cpp',
-          'haskell',
-          'zig',
-          'hcl',
-          'terraform',
-          'proto',
-          'json',
-          'javascript',
-          'typescript',
-          'tsx',
-          'yaml',
-          'toml',
-          'html',
-          'css',
-          'tsv',
-          'csv',
-          'xml',
-          'markdown',
-          'markdown_inline',
-          'graphql',
-          'bash',
-          'lua',
-          'vim',
-          'dockerfile',
-          'gitignore',
-          'git_config',
-          'git_rebase',
-          'gitcommit',
-          'diff',
-          'ssh_config',
-          'sxhkdrc',
-        },
+        ensure_installed = {},
+        auto_install = true, -- need the `tree-sitter` CLI installed locally
         incremental_selection = {
           enable = true,
           keymaps = {
             init_selection = '<M-n>',
             node_incremental = '<M-n>',
-            scope_incremental = false,
+            scope_incremental = '<M-c>',
             node_decremental = '<M-p>',
           },
         },
+        textobjects = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['aa'] = '@paramater.outer', -- 'a' for argument
+            ['ia'] = '@paramater.inner',
+            ['ac'] = '@comment.outer',
+            ['ic'] = '@comment.inner',
+            ['as'] = '@class.outer', -- 's' for struct
+            ['is'] = '@class.inner',
+            ['al'] = '@loop.outer',
+            ['il'] = '@loop.inner',
+            ['ai'] = '@call.outer', -- 'i' for invocation
+            ['ii'] = '@call.inner',
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']f'] = '@function.outer',
+              [']a'] = '@parameter.inner',
+              [']c'] = '@comment.outer',
+              [']s'] = '@class.outer',
+              [']l'] = '@loop.outer',
+              [']i'] = '@call.inner',
+            },
+            goto_next_end = {},
+            goto_previous_start = {
+              ['[f'] = '@function.outer',
+              ['[a'] = '@parameter.inner',
+              ['[c'] = '@comment.outer',
+              ['[s'] = '@struct.outer',
+              ['[l'] = '@loop.outer',
+              ['[i'] = '@call.inner',
+            },
+            goto_previous_end = {},
+            -- Below will go to either the start or the end, whichever is closer.
+            -- Use if you want more granular movements
+            -- Make it even more gradual by adding multiple queries and regex.
+            goto_next = {},
+            goto_previous = {},
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>za'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>zA'] = '@parameter.inner',
+            },
+          },
+        },
       }
+
+      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+      -- vim way: ; goes to the direction you were moving.
+      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
+      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
+      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+      vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f)
+      vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F)
+      vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t)
+      vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T)
     end,
   },
 }
