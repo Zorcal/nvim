18afcc90d18587901de0537ae026a46605b78cf6
Zorcal <zorcal.dev@gmail.com>
2024-01-07 09:07:06 +0100

    update formatters

--- lua/zorcal/plugins/conform.lua
+++ lua/zorcal/plugins/conform.lua
@@ -7,19 +7,29 @@ return {
 
     conform.setup {
       formatters_by_ft = {
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescriptreact = { 'prettier' },
-        css = { 'prettier' },
-        html = { 'prettier' },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
-        markdown = { 'prettier' },
-        graphql = { 'prettier' },
+        javascript = { { 'prettierd', 'prettier' }, 'eslint_d' },
+        typescript = { { 'prettierd', 'prettier' }, 'eslint_d' },
+        javascriptreact = { { 'prettierd', 'prettier' }, 'eslint_d' },
+        typescriptreact = { { 'prettierd', 'prettier' }, 'eslint_d' },
+        css = { { 'prettierd', 'prettier' } },
+        html = { { 'prettierd', 'prettier' } },
+        json = { { 'prettierd', 'prettier' } },
+        yaml = { { 'prettierd', 'prettier' } },
+        markdown = { { 'prettierd', 'prettier' } },
+        graphql = { { 'prettierd', 'prettier' } },
         lua = { 'stylua' },
         python = { 'isort', 'black' },
-        go = { 'gofumpt', 'goimports' },
+        go = {
+          -- See formatter_args below. golines is a wrapper around gofumpt.
+          'golines',
+          'goimports',
+        },
+        rust = { 'rustfmt' },
+        toml = { 'taplo' },
+        sql = { 'sql_formatter' },
+        sh = { 'shellcheck', 'shfmt' },
+        bash = { 'shellcheck', 'shfmt' },
+        tf = { 'terraform_fmt' },
       },
       format_on_save = {
         lsp_fallback = true,
@@ -28,9 +38,16 @@ return {
       },
     }
 
-    require('conform.util').add_formatter_args(require 'conform.formatters.goimports', {
+    local util = require 'conform.util'
+    util.add_formatter_args(require 'conform.formatters.goimports', {
       '-local',
       'github.com/formulatehq',
+    })
+    util.add_formatter_args(require 'conform.formatters.golines', {
+      '--base-formatter',
+      'gofumpt',
+      '-m',
+      '150',
     })
   end,
 }
