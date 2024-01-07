2971534cd3bee73d41d5277d9ef160c8d7a1dcd3
Zorcal <zorcal.dev@gmail.com>
2024-01-07 09:07:07 +0100

    fix path bug in qf filter test predicate

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -3,33 +3,35 @@ local str = require 'zorcal.funcs.str'
 
 local opts = { noremap = true, silent = true, buffer = true }
 
+local function filter_tests_predicate(entry)
+  local test_file_suffixes = {
+    -- Go.
+    '_test.go',
+    -- JavaScript ecosystem.
+    'test.ts',
+    'test.js',
+    'test.jsx',
+    'test.tsx',
+    'spec.js',
+    'spec.jsx',
+    'spec.ts',
+    'spec.tsx',
+  }
+
+  local testdata_dirs = {
+    -- Go
+    'testdata/',
+    -- JavaScript ecosystem.
+    '__tests__/',
+    '__snapshots__/',
+  }
+
+  local path = vim.api.nvim_buf_get_name(entry.bufnr)
+  return not str.ends_with_any(path, test_file_suffixes) and not str.contains_any(path, testdata_dirs)
+end
+
 vim.keymap.set('n', 'dd', qf.delete_entry, opts)
 vim.keymap.set('v', 'd', qf.delete_visual_selection, opts)
 vim.keymap.set('n', 'ft', function()
-  qf.filter(function(entry)
-    local test_file_suffixes = {
-      -- Go.
-      '_test.go',
-      -- JavaScript ecosystem.
-      'test.ts',
-      'test.js',
-      'test.jsx',
-      'test.tsx',
-      'spec.js',
-      'spec.jsx',
-      'spec.ts',
-      'spec.tsx',
-    }
-
-    local testdata_dirs = {
-      -- Go
-      'testdata/',
-      -- JavaScript ecosystem.
-      '__tests__/',
-      '__snapshots__/',
-    }
-
-    local path = entry.text
-    return not str.ends_with_any(path, test_file_suffixes) and not str.contains_any(path, testdata_dirs)
-  end)
+  qf.filter(filter_tests_predicate)
 end, opts)
