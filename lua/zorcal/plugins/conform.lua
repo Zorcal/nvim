9f475c33c30b156b4560acb5b22c303c43abb3b8
Zorcal <zorcal.dev@gmail.com>
2024-02-26 09:55:44 +0100

    update conform formatting config

--- lua/zorcal/plugins/conform.lua
+++ lua/zorcal/plugins/conform.lua
@@ -26,17 +26,38 @@ return {
         },
         rust = { 'rustfmt' },
         toml = { 'taplo' },
-        -- sql = { 'sql_formatter' },
+        sql = { 'sql_formatter' },
         sh = { 'shellcheck', 'shfmt' },
         bash = { 'shellcheck', 'shfmt' },
         tf = { 'terraform_fmt' },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      format_on_save = function(bufnr)
+        local ignore_filetypes = { 'sql' }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match '/node_modules/' then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true, async = false }
+      end,
     }
+
+    vim.api.nvim_create_user_command('Format', function(args)
+      local range = nil
+      if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+          start = { args.line1, 0 },
+          ['end'] = { args.line2, end_line:len() },
+        }
+      end
+      require('conform').format { async = true, lsp_fallback = true, range = range }
+    end, { range = true })
 
     local util = require 'conform.util'
     util.add_formatter_args(require 'conform.formatters.goimports', {
