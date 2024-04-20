df93e5917416d2d31d228f71066e83392966a72b
Zorcal <zorcal.dev@gmail.com>
2024-04-20 10:10:36 +0200

    add blame.nvim plugin

---
+++ lua/zorcal/plugins/blame.lua
@@ -0,0 +1,8 @@
+return {
+  'FabijanZulj/blame.nvim',
+  config = function()
+    require('blame').setup()
+
+    vim.keymap.set('n', '<leader>gb', '<cmd>BlameToggle window<CR>', { desc = 'Toggle Git blame' })
+  end,
+}
